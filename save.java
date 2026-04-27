698296608b2f7f7a01af84b1d0fcbe8d55132cb2
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-04-28 01:20:35 +0530

    Update save.java

--- save.java
+++ save.java
@@ -48,62 +48,104 @@ Output: [3, 35]
 Explanation: The minimum element is 3, and the maximum element is 35.
 // */
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
-class GfG {
-    public static ArrayList<Integer> findMinMax(int[] arr) {
-        int mini = Integer.MAX_VALUE;
-        int maxi = Integer.MIN_VALUE;
+// class GfG {
+//     public static ArrayList<Integer> findMinMax(int[] arr) {
+//         int mini = Integer.MAX_VALUE;
+//         int maxi = Integer.MIN_VALUE;
         
-        // Find minimum and maximum
-        for (int num : arr) {
-            if (num < mini) mini = num;
-            if (num > maxi) maxi = num;
-        }
+//         // Find minimum and maximum
+//         for (int num : arr) {
+//             if (num < mini) mini = num;
+//             if (num > maxi) maxi = num;
+//         }
         
-        ArrayList<Integer> result = new ArrayList<>();
-        result.add(mini);
-        result.add(maxi);
-        return result;
+//         ArrayList<Integer> result = new ArrayList<>();
+//         result.add(mini);
+//         result.add(maxi);
+//         return result;
     
-}
+// }
 
-    public static void main(String[] args) {
-        int[] arr = {3, 5, 4, 1, 9};
-        ArrayList<Integer> result = findMinMax(arr);
-        System.out.println(result.get(0) + " " + result.get(1));
-          }
-}
+//     public static void main(String[] args) {
+//         int[] arr = {3, 5, 4, 1, 9};
+//         ArrayList<Integer> result = findMinMax(arr);
+//         System.out.println(result.get(0) + " " + result.get(1));
+//           }
+// }
 
-class Solution {
-    public void sort012(int[] arr) {
-        // algorithm --> count 0, 1, 2
-        int countz = 0, counto = 0, countt = 0;
-        // then insert in same array --> 0, 1, 2
-        for(int num : arr) {
-            if(num == 0) countz++;
-            else if(num == 1) counto++;
-            else countt++;
-        }
+
+// import java.util.Arrays;
+// import java.util.Scanner;
+
+// class Solution {
+//     public static int kthSmallest(int[] arr, int k) {
+//         Arrays.sort(arr);
+//         return arr[k - 1];
+//     }
+
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+//         for(int i = 0; i < arr.length; i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         int k = sc.nextInt();
+//         int result = kthSmallest(arr, k);
+//         System.out.println(result);
+//     }
+// }
+
+
+// class Solution {
+//     public void sort012(int[] arr) {
+//         // algorithm --> count 0, 1, 2
+//         int countz = 0, counto = 0, countt = 0;
+//         // then insert in same array --> 0, 1, 2
+//         for(int num : arr) {
+//             if(num == 0) countz++;
+//             else if(num == 1) counto++;
+//             else countt++;
+//         }
         
-        // now inserting element's till count size
-        for(int i = 0 ; i < countz; i++){      
-            arr[i] = 0;
-        }
-        
-        for(int i = countz; i < countz +  counto; i++){
-            arr[i] = 1;
-        }
-        for(int i = countz + counto ; i < countz + counto + countt; i++){
-            arr[i] = 2;
-        }
-   
- }
-       
-}
+//         // now inserting element's till count size
+//         for(int i = 0 ; i < countz; i++){
+//             arr[i] = 0;
+//         }
+//         for(int i = countz; i < countz + counto; i++){
+//             arr[i] = 1;
+//         }
+//         for(int i = countz + counto ; i < countz + counto + countt; i++){
+//             arr[i] = 2;
+//         }
+//     }
+// }
 
 
+// 4 --> Move all negative number's to the left side of an array 
+// Java program to Move all negative numbers
+// to beginning and positive to end
+/*
+Input: -12, 11, -13, -5, 6, -7, 5, -3, -6
+Output: -12 -13 -5 -7 -3 -6 11 6 5
+*/
+// By using two pointer approach 
 
+// import java.util.Scanner;
 
+// class save{
+//     static int[] move(int[] arr){
+//         int left = 0, right = arr.length - 1;
+//         while(left < right){
+//             // jb left m -ve ho tb left ko increase nhi krna bcz hume left m -ve lane h
+//             if(arr[left] < 0) left++;
 
-  
+//             else if(arr[right] > 0) right--;
+
+//             // baki bacha test case swap 
+//             int temp = arr[left];
+//             arr[left] = arr[right];
+//             arr[right] = temp;
+//   
