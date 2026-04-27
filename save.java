1a104149996a3ed04d018eec5484e46eeab8bcb3
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-04-28 01:23:25 +0530

    Update save.java

--- save.java
+++ save.java
@@ -148,4 +148,10 @@ Output: -12 -13 -5 -7 -3 -6 11 6 5
 //             int temp = arr[left];
 //             arr[left] = arr[right];
 //             arr[right] = temp;
-//   
+//         }
+//         return arr;
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr = {-12, 11, -13, -5, 6, -7, 5, -3, -6};
+//    
