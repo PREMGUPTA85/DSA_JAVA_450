10436b575b007c6c40fda11d54f0ae2a135007df
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-04-28 01:23:53 +0530

    Comment out main method and related code

--- save.java
+++ save.java
@@ -154,4 +154,6 @@ Output: -12 -13 -5 -7 -3 -6 11 6 5
 
 //     public static void main(String[] args) {
 //         int[] arr = {-12, 11, -13, -5, 6, -7, 5, -3, -6};
-//    
+//   int[] ans = move(arr);
+
+//  
