8ec67c95e62945c8e2a4552893de2c27677ba529
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-04-28 01:24:18 +0530

    Uncomment test code for move function
    Uncommented code for testing the move function with an array.

--- save.java
+++ save.java
@@ -156,4 +156,6 @@ Output: -12 -13 -5 -7 -3 -6 11 6 5
 //         int[] arr = {-12, 11, -13, -5, 6, -7, 5, -3, -6};
 //   int[] ans = move(arr);
 
-//  
+//         for (int num : ans) {
+//             System.out.print(num + " ");
+
