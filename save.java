9672f572f37de27358db36ada24055e827235098
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-04-28 01:25:35 +0530

    Update save.java

--- save.java
+++ save.java
@@ -146,6 +146,23 @@ Output: -12 -13 -5 -7 -3 -6 11 6 5
 
 //             // baki bacha test case swap 
 //             int temp = arr[left];
+//             arr[left] = arr[right];
+//             arr[right] = temp;
+//         }
+//         return arr;
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr = {-12, 11, -13, -5, 6, -7, 5, -3, -6};
+//   int[] ans = move(arr);
+
+//         for (int num : ans) {
+//             System.out.print(num + " ");
+
+//         }
+
+//         System.out.println();
+//     }
 
 // }
 
