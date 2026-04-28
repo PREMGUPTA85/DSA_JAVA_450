3cb8d240e5f343aece5fba258e15e34602c7d9bd
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-04-28 22:58:42 +0530

    Update save.java

--- save.java
+++ save.java
@@ -154,15 +154,20 @@ Output: -12 -13 -5 -7 -3 -6 11 6 5
 
 //     public static void main(String[] args) {
 //         int[] arr = {-12, 11, -13, -5, 6, -7, 5, -3, -6};
-//   int[] ans = move(arr);
+//         int[] ans = move(arr);
 
 //         for (int num : ans) {
 //             System.out.print(num + " ");
-
 //         }
-
 //         System.out.println();
 //     }
-
 // }
 
+
+
+// ------------------------(String)------------------------
+// Reverse String 
+// class Solution {
+//     public void reverseString(char[] s) {
+//         int i = 0, j = s.length - 1;
+//   
