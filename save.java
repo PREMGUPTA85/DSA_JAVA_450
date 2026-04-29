2630289a82144864c33ea4205f394472684af4ac
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-04-29 23:49:29 +0530

    Update save.java

--- save.java
+++ save.java
@@ -254,8 +254,8 @@ now i = i + count = 0 + 3 = 3
 i = 5 --> g -->  
 
 5 + 1 --> 6 < 11 && g == g
-
-
+arr[i] == arr[i + count] i = 4 --> i + count < 11 -->  f --
+4 + 1 --
 
 // agar count 1 se bda h to 
 */
