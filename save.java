cb04118c3e189f67e96748ac0f9a251720c9ee46
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-04-29 23:51:02 +0530

    Update save.java

--- save.java
+++ save.java
@@ -259,6 +259,8 @@ arr[i] == arr[i + count] i = 4 --> i + count < 11 -->  f --
 // iska mtlb equal nhi h
  to exit ho jao ab bahar aa jao 
 i = i arr[i] == arr[i + count] i = 4 --> i + count < 11 -->  f --
+4 + 1 --> 5 < 11 && 4 != 4 + 1 --> 4 !=5
+
 
 
 // agar count 1 se bda h to 
